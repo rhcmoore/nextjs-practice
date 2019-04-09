4ccb5276f9457a103780d2a2d77469483bcb4e56
Ryan Moore <rhcmoore@gmail.com>
2019-04-09 15:17:08 -0700

    Initial commit

---
+++ .next/static/development/pages/auth.js
@@ -0,0 +1,95 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/auth.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fauth&absolutePagePath=%2FUsers%2Fryanmoore%2FDocuments%2FUdemy%2FReact%2Fnextjs%2Fpages%2Fauth%2Findex.js!./":
+/*!****************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fauth&absolutePagePath=%2FUsers%2Fryanmoore%2FDocuments%2FUdemy%2FReact%2Fnextjs%2Fpages%2Fauth%2Findex.js ***!
+  \****************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/auth", function() {
+      var page = __webpack_require__(/*! ./pages/auth/index.js */ "./pages/auth/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/auth/index.js */ "./pages/auth/index.js", function() {
+          if(!next.router.components["/auth"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/auth/index.js */ "./pages/auth/index.js")
+          next.router.update("/auth", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_1aef2d0bbc0d334d831c ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_1aef2d0bbc0d334d831c */ "dll-reference dll_1aef2d0bbc0d334d831c"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/auth/index.js":
+/*!*****************************!*\
+  !*** ./pages/auth/index.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/ryanmoore/Documents/Udemy/React/nextjs/pages/auth/index.js";
+
+
+var authIndexPage = function authIndexPage() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "Auth Index Page"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (authIndexPage);
+
+/***/ }),
+
+/***/ 1:
+/*!********************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fauth&absolutePagePath=%2FUsers%2Fryanmoore%2FDocuments%2FUdemy%2FReact%2Fnextjs%2Fpages%2Fauth%2Findex.js ***!
+  \********************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fauth&absolutePagePath=%2FUsers%2Fryanmoore%2FDocuments%2FUdemy%2FReact%2Fnextjs%2Fpages%2Fauth%2Findex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fauth&absolutePagePath=%2FUsers%2Fryanmoore%2FDocuments%2FUdemy%2FReact%2Fnextjs%2Fpages%2Fauth%2Findex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_1aef2d0bbc0d334d831c":
+/*!*******************************************!*\
+  !*** external "dll_1aef2d0bbc0d334d831c" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_1aef2d0bbc0d334d831c;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]));;
+//# sourceMappingURL=auth.js.map
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const indexPage = () => (
+    <div>
+        <h1>Main Page</h1>
+    </div>
+);
+
+export default indexPage;
